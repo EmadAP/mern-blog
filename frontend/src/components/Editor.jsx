45e87f6baf5600680c5a4a40fad9082071fa4fd5
EmadAP <emad.amoupour1997@gmail.com>
2025-04-24 22:51:02 +0330

    App phase one complete

---
+++ frontend/src/components/Editor.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import ReactQuill from "react-quill-new";
+
+const modules = {
+  toolbar: [
+    [{ header: [1, 2, false] }],
+    ["bold", "italic", "underline", "strike", "blockquote"],
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ],
+    ["link", "image"],
+    ["clean"],
+  ],
+};
+const formats = [
+  [
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "align",
+    "list",
+    "indent",
+    "size",
+    "header",
+    "link",
+    "image",
+    "video",
+    "color",
+    "background",
+  ],
+];
+
+function Editor({ value, onChange }) {
+  return (
+    <ReactQuill
+      value={value}
+      onChange={onChange}
+      modules={modules}
+      formats={formats}
+    />
+  );
+}
+
+export default Editor;
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,52 @@
+import React, { useContext, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { UserContext } from "./UserContext";
+
+function Header() {
+  //const [username, setUsername] = useState(null);
+  const { setUserInfo, userInfo } = useContext(UserContext);
+  useEffect(() => {
+    fetch("http://localhost:4000/profile", {
+      credentials: "include",
+    }).then((response) => {
+      response.json().then((userInfo) => {
+        //setUsername(userInfo.username);
+        setUserInfo(userInfo);
+      });
+    });
+  }, [setUserInfo]);
+
+  function logout() {
+    fetch("http://localhost:4000/logout", {
+      credentials: "include",
+      method: "POST",
+    });
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;
+
+  return (
+    <header>
+      <Link to="/" className="logo">
+        MyBlog
+      </Link>
+      <nav>
+        {username && (
+          <>
+            <Link to="/create">Create new post</Link>
+            <Link onClick={logout}>Logout</Link>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
